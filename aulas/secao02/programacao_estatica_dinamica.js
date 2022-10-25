6bb2863d7df3ad7e59cce04dd45d86ea467d6c91
Edson Pimenta <edson.tibo@gmail.com>
2022-10-24 21:44:01 -0300

    Anotações sobre programação estática e dinâmica

---
+++ aulas/secao02/programacao_estatica_dinamica.js
@@ -0,0 +1,24 @@
+/*
+Programação Estática e Dinâmica
+
+Usamos o termo programação estática ou
+dinâmica para nos referir do uso de
+linguagens de programação que não são
+processados no servidor (estática) ou
+que são processados no servidor (dinâmica).
+
+Como exemplos de linguagens estáticas temos:
+  - HTML;
+  - CSS;
+  - JavaScript;
+  - etc...
+
+Como exemplos de linguagens dinâmicas temos:
+  - Python;
+  - Java;
+  - Ruby;
+  - PHP;
+  - C#;
+  - JavaScript;
+  - etc...
+ */
