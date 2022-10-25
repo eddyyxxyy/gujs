2a5ddeb8ac5a22338cd61af98dc7088c0d283b52
Edson Pimenta <edson.tibo@gmail.com>
2022-10-24 21:04:38 -0300

    Anotações sobre cliente-servidor

---
+++ aulas/secao02/arquitetura_cliente_servidor.js
@@ -0,0 +1,33 @@
+/*
+Arquitetura Cliente Servidor
+
+Definição formal:
+
+"Estrutura de computação distribuída que distribui
+tarefas e cargas de trabalho entre fornecedores de
+serviço, chamados servidores, e os requerentes
+dos serviços, chamados clientes."
+
+Por que é importante falarmos sobre isso?
+
+Pois, estamos falando da criação de sistemas
+para Web, sobre programação Web.
+
+Ou seja, as pessoas que vão utilizar/acessar
+seu sistema, usarão "clientes" para fazê-lo,
+se conectando ao sistema que está em um servidor.
+
+Entretanto, vale a pena ressaltar que os
+clientes não são os computadores, celulares e/ou
+portáteis, estes são os dispositivos usados pelas
+pessoas. Essas pessoas, utilizaram, por exemplo,
+um navegador, sendo de fato os clientes.
+
+O meio, a forma de conexão, é a ‘internet’, a rede
+mundial de computadores. Mas isso não quer dizer
+que a arquitetura cliente-servidor é exclusiva
+e somente funciona por esses meios, podemos na
+nossa máquina termos o cliente e servidor, para
+podermos desenvolver de forma concisa, rápida e
+contínua.
+ */
