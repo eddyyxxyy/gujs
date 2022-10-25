50c2dac928c810a69e4047c897fec6dad91f14b7
Edson Pimenta <edson.tibo@gmail.com>
2022-10-24 21:04:55 -0300

    Anotações sobre HTTP e seus métodos

---
+++ aulas/secao02/protocolo_http_e_seus_verbos.js
@@ -0,0 +1,58 @@
+/*
+Protocolo HTTP e seus verbos
+
+Quando digitamos um endereço 'Web',
+muitas das vezes nem colocamos o
+termo "http" ou "https", pois os
+navegadores geralmente já preenchem
+esse termo faltante para o acesso.
+
+O HTTP é o "próprio nome" do serviço
+'Web', pois este (o serviço Web) é baseado
+nesse protocolo.
+
+O HTTP é um dos protocolos mais importantes
+e utilizados no mundo. Uma tradução para o
+termo seria "Protocolo de Transferência de
+Hipertexto" e, mesmo sem saber, o utilizamos
+para fazer acesso a todos os Websites e redes
+sociais que conhecemos.
+
+Os serviços que implementam o protocolo
+são chamados Serviços HTTP ou Servidores
+HTTP se comunicam nas portas 80 para HTTP
+ou 443 para HTTPS.
+
+
+Os Verbos do protocolo HTTP são os métodos
+que indicam a ação a ser realizada no recurso
+identificado.
+
+Estes métodos são chamados Verbos HTTP
+quando utilizados como aplicações Web.
+
+Alguns exemplos desses métodos/verbos:
+  - GET -> Busca recursos, cache;
+  - POST -> Criar um novo recurso;
+  - PUT/PATCH -> Atualizar um recurso existente;
+  - DELETE -> Remover um recurso.
+
+Quando queremos acessar um site, estamos
+realizando um GET, solicitando ao navegador
+que utilize esse verbo.
+
+Quando cadastramos nossas informações,
+em um site ou formulário, estamos solicitando
+ao navegador que realize um POST.
+
+Quando atualizamos nossos dados ou
+qualquer outra informação que já está
+cadastrada, estamos utilizando o
+PUT/PATCH. Utilizando o PUT quando
+atualizamos todos os dados e o PATCH
+quando atualizamos parte dos dados.
+
+Quando utilizamos o DELETE, temos um
+recurso armazenado que desejamos excluir,
+deletar.
+ */
