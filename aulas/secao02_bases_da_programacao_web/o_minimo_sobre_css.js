de9a86f1f0cd0b78e2f06c0aaa073e3cd19b25c5
Edson Pimenta <edson.tibo@gmail.com>
2022-10-26 16:34:34 -0300

    Anotações e exemplos sobre CSS3

---
+++ aulas/secao02_bases_da_programacao_web/o_minimo_sobre_css.js
@@ -0,0 +1,76 @@
+/*
+O mínimo que você precisa saber de CSS
+
+CSS é a sigla de Cascading Estilo Sheets, ou
+Folha de Estilo em Cascata sendo usado para
+aplicar estilos nas páginas HTML.
+
+Desenvolvido pelo W3C em 1996 para formatar as
+páginas HTML, já que as tags HTML foram
+projetadas para realizar marcação das páginas.
+
+A relação entre HTML e CSS é bem forte. Como o
+HTML é uma linguagem de marcação (o alicerce
+de um site) e o CSS é focado no estilo (toda
+a estética de um site), eles andam juntos.
+
+CSS não é tecnicamente uma necessidade, mas
+provavelmente você não gostaria de olhar para um
+site que usa apenas HTML, pois isso pareceria
+completamente abandonado.
+
+
+Basicamente existem duas formas de se adicionar
+estilos CSS em uma página HTML:
+
+1 - Inline, ou seja, diretamente na página HTML
+(não recomendado);
+
+2 - Externo, ou seja, em arquivo externo ou mesmo
+diretamente da ‘internet’ (recomendado);
+
+
+Usamos CSS para formatar nossas páginas HTML de 3
+formas diferentes:
+
+1 - Por elemento (tag) html: Usado quando queremos
+que todas as tags especificadas recebem a mesma
+formatação;
+
+2 - Por classe: Usado quando queremos que tod0
+elemento que incorporar a determinada classe receba
+a formatação;
+
+3 - Por id: Usado quando queremos que elementos
+individuais recebem a formatação.
+
+
+Anatomia de um comando CSS:
+
+Um comando básico CSS é composto por seletor e
+declarações que contém propriedade e valor.
+
+SELETOR {
+  propriedade: valor;
+}
+
+O seletor seleciona quais elementos da página
+receberão a formatação da propriedade com o valor
+especificado. Vimos a pouco que o seletor pode
+ser uma tag HTML, uma classe ou um id.
+
+São muitas as propriedades e valores possíveis
+para formatação de uma página. Podemos formatar
+praticamente qualquer coisa, lembrando que
+estamos falando de aplicação de estilos, que
+envolve escolher tipo/tamanho/cores de fonte,
+cores de páginas, alinhamento, posicionamento
+de seções, etc.
+
+Programador backend sofre com estas coisas.
+
+Para observar os exemplos de utilização do css
+vá para o diretório:
+
+aulas/secao02_bases_da_programacao_web/exemplos/css
+ */
