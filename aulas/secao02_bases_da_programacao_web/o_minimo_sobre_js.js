08bc33aa19102acbe56780acf47d441899db91ff
Edson Pimenta <edson.tibo@gmail.com>
2022-10-26 17:11:00 -0300

    Anotações e exemplos sobre JS

---
+++ aulas/secao02_bases_da_programacao_web/o_minimo_sobre_js.js
@@ -0,0 +1,27 @@
+/*
+O mínimo que você precisa saber de JavaScript
+
+A linguagem JavaScript foi criada por Brendan Eich
+na empresa Netscape.
+
+Atualmente é mantida pela European Computer Manufacturer’s
+Association e seu nome “oficial" é ECMAScript, mas continua
+sendo mais conhecida, principalmente pelos mais leigos, como
+JavaScript.
+
+OBS: JavaScript não é Java!
+
+"Java is to JavaScript as Ham is for Hamster"
+
+O JavaScript completa a tríade da Web se encarregando dos
+comportamentos e ações das páginas.
+
+HTML -> Estrutura;
+CSS -> Estilos;
+JavaScript -> Comportamento/ações;
+
+Vamos entender melhor sobre comportamentos/ações nos
+exemplos no diretório:
+
+aulas/secao02_bases_da_programacao_web/exemplos/js
+ */
