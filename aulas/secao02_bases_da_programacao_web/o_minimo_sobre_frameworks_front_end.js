682a81033584d688804ad24fda865d8abc0034ec
Edson Pimenta <edson.tibo@gmail.com>
2022-10-26 17:31:29 -0300

    Anotação e exemplo sobre frameworks Front-End

---
+++ aulas/secao02_bases_da_programacao_web/o_minimo_sobre_frameworks_front_end.js
@@ -0,0 +1,17 @@
+/*
+O mínimo que você precisa saber de Frameworks Front-End
+
+Um framework frontend nada mais é do que um conjunto de
+ferramentas que oferecem diversos recursos para desenvolvedores
+frontend, ou seja, para realizar as criações de html, css e javascript.
+
+Um dos frameworks mais famosos e utilizamos atualmente é o Bootstrap[1]
+criado pela empresa por trás do Twitter e disponibilizado para toda a
+comunidade.
+
+Para visualizar exemplos sobre frameworks front-end, vá para o diretório:
+
+aulas/secao02_bases_da_programacao_web/exemplos/frameworks
+
+https://getbootstrap.com.br
+ */
