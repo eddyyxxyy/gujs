fb170e6552346606af9a0c963b10dfd270bad225
Edson Pimenta <edson.tibo@gmail.com>
2022-11-01 09:45:47 -0300

    Anotações iniciais sobre JS

---
+++ aulas/secao03_introducao_js/introducao_ao_javascript.js
@@ -0,0 +1,25 @@
+/*
+JavaScript é uma linguagem de programação
+que permite que scripts possam ser executados
+a partir do cliente.
+
+Mas o que são scripts?
+
+Scripts, nada mais são do que programas em texto,
+escritos em alguma linguagem de programação que
+não é compilada em linguagem de máquina para ser
+executado. Sempre interpretado linha a linha e
+quem tem a função de executar o script é o navegador
+‘web’; o navegador interpreta e renderiza HTML e CSS,
+por exemplo.
+
+Os clientes são os navegadores, Firefox, Chrome, Opera,
+Internet Explorer...
+
+E, onde o JS é usado?
+
+Se existe uma linguagem que hoje em dia é onipresente,
+é a JS. Ela está presente em tod0 lugar, em toda ‘internet’.
+
+Para executar o JS, simplesmente precisamos do navegador.
+ */
